88c48e626837334fa18e326c92989095e6601be5
Joshua-Fish-Student <jofid7d@student.oaisd.org>
2024-10-21 16:48:00 +0000

    switched to case

--- Program.cs
+++ Program.cs
@@ -7,41 +7,42 @@ public class Program{
         int number = int.Parse(Console.ReadLine());
         if (number >= 0 && number <=10){
             Console.Write("Your number is ");
-        }
-        if (number == 1){
-            Console.WriteLine("One.");
-        }
-        else if (number == 2){
-            Console.WriteLine("Two.");
-        }
-        else if (number == 3){
-            Console.WriteLine("Three.");
-        }
-        else if (number == 4){
-            Console.WriteLine("Four.");
-        }
-        else if (number == 5){
-            Console.WriteLine("Five.");
-        }
-        else if (number == 6){
-            Console.WriteLine("Six.");
-        }
-        else if (number == 7){
-            Console.WriteLine("Seven.");
-        }
-        else if (number == 8){
-            Console.WriteLine("Eight.");
-        }
-        else if (number == 9){
-            Console.WriteLine("Nine.");
-        }
-        else if (number == 10){
-            Console.WriteLine("Ten.");
-        }
-        else if (number == 0){
-            Console.WriteLine("Zero.");
-        }
-        else{
+            switch (number){
+                case 1:
+                    Console.WriteLine("One.");
+                    break;
+                case 2:
+                    Console.WriteLine("Two.");
+                    break;
+                case 3:
+                    Console.WriteLine("Three.");
+                    break;
+                case 4:
+                    Console.WriteLine("Four.");
+                    break;
+                case 5:
+                    Console.WriteLine("Five.");
+                    break;
+                case 6:
+                    Console.WriteLine("Six.");
+                    break;
+                case 7:
+                    Console.WriteLine("Seven.");
+                    break;
+                case 8:
+                    Console.WriteLine("Eight.");
+                    break;
+                case 9:
+                    Console.WriteLine("Nine.");
+                    break;
+                case 10:
+                    Console.WriteLine("Ten.");
+                    break;
+                case 0:
+                    Console.WriteLine("Zero.");
+                    break;
+            }
+        } else{
             Console.WriteLine("That number is not in range.");
         }
     }
